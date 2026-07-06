9a6fa96fe41effdf1201c2d78f9b23be071a61d9
Jittrin <Jittrin.Puttawetmongkol@gmail.com>
2026-07-06 17:10:14 +0700

    complete EX4

--- query4.mongodb.js
+++ query4.mongodb.js
@@ -1,11 +1,11 @@
 //ตั้งชื่อไฟล์ให้ javascript สามารถใช้ extension กับ mongodb ได้ ต้องตั้งชื่อให้มี .mongodb.js
 //สร้าง function ที่ข้างในเป็นข้อมูล database ชื่อ sample_mflix
 use("sample_mflix")
-//ex.3  topic1
-db.theaters.find({"location.address.state": "AL"}).count();
+//ex.4  topic1
+db.movies.find({plot : {$regex : "American"}}).count();
 
-//ex.3  topic2
-db.theaters.find({"location.address.city": "La Quinta"}).count();
+//ex.4  topic2
+db.movies.find({plot : {$regex : "the word street.$"}}).count();
 
-//ex.3  topic3
-db.theaters.find({"location.address.city": "La Quinta"});
+//ex.4  topic3
+db.movies.findOne({plot : {$regex : "the word street.$"}});
