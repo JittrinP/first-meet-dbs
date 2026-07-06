a6342182a09cbcb9527dff13d1e71f8567a0446a
Jittrin <Jittrin.Puttawetmongkol@gmail.com>
2026-07-06 15:54:24 +0700

    complete EX1

--- query.mongodb.js
+++ query.mongodb.js
@@ -1,7 +1,17 @@
 //ตั้งชื่อไฟล์ให้ javascript สามารถใช้ extension กับ mongodb ได้ ต้องตั้งชื่อให้มี .mongodb.js
 //สร้าง function ที่ข้างในเป็นข้อมูล database ชื่อ sample_mflix
 use("sample_mflix")
-//อันนี้เป็น function ที่ไปค้นหา collection ทั้งหมดที่มีชื่อว่า comments
-// db.comments.find();
+//อันนี้เป็น function ที่ไปค้นหา collection ทั้งหมดที่มีชื่อว่า comments ex.1  topic1
 
-db.comments.find({_id:ObjectId("5a9427648b0beebeb69579f5")});
+db.comments.find();
+
+// การหา id นี้โดยเอามาตัวเดียว ex.1 topic2
+db.comments.findOne({_id:ObjectId("5a9427648b0beebeb69579f5")});
+
+// การหา email นี้โดยเอามาตัวเดียว ex.1 topic3
+db.comments.findOne({email : "john_bishop@fakegmail.com"});
+
+// การหา email นี้โดยเอามาตัวเดียว ex.1 topic4
+db.comments.findOne({name: "John Bishop"});
+
+//db.movies.find({ type: "movie" , rated :"TV-G"}).count();
---
+++ query.mongodb2.js
@@ -0,0 +1,5 @@
+//ตั้งชื่อไฟล์ให้ javascript สามารถใช้ extension กับ mongodb ได้ ต้องตั้งชื่อให้มี .mongodb.js
+//สร้าง function ที่ข้างในเป็นข้อมูล database ชื่อ sample_mflix
+use("sample_mflix")
+//ex.2  topic1
+
